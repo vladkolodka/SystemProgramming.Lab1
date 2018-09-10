3f31f8d726e22a78c5e8e19028cd2bf9827d0811
Vladyslav <vladkolodka@gmail.com>
2018-09-10 20:57:22 +0300

    Add project files.

---
+++ SystemProgramming.Lab1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemProgramming.Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path1 = @"C:\Users\vladk\Desktop\1.txt";
+            var path2 = @"C:\Users\vladk\Desktop\2.txt";
+
+            var result = Api.CopyFile(path1, path2, false);
+        }
+
+        public static class Api
+        {
+            [DllImport("kernel32.dll")]
+            public static extern bool CopyFile(string path1, string path2, bool fail);
+        }
+    }
+}
